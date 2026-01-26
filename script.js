d7a277104b2ccb0faed50ac69675cceb23ba36c1
shahzad007-crypto <shahzad007_english@yahoo.com>
2026-01-26 22:18:04 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-https://script.google.com/macros/s/AKfycbyGgWbpz3lJTS4xfYTu8Ld3MI2mKAvCm534o191QO_PCL1yX7AiCewQElL3URTZJGNcKw/exec
+const WEB_APP_URL = "https://script.google.com/macros/s/AKfycbzWtKAWn6ymYoYB2K994mnxKojN0kSHC3PPsa7K_hhOlUBFGWINh7sN0AhZBfJl95-lZg/exec"; // your URL
+
+document.getElementById("uploadForm").addEventListener("submit", function(e){
+  e.preventDefault();
+
+  const name = document.getElementById("name").value;
+  const sid = document.getElementById("sid").value;
+  const skill = document.getElementById("skill").value;
+  const fileInput = document.getElementById("file");
+
+  let fileName = fileInput.files[0] ? fileInput.files[0].name : "No file";
+
+  const data = new URLSearchParams();
+  data.append("name", name);
+  data.append("sid", sid);
+  data.append("skill", skill);
+  data.append("file", fileName);
+
+  fetch(WEB_APP_URL, { method: "POST", body: data })
+    .then(res => res.text())
+    .then(msg => {
+      document.getElementById("msg").innerText = "✅ Submission stored in Google Sheet!";
+      document.getElementById("uploadForm").reset();
+    })
+    .catch(err => {
+      document.getElementById("msg").innerText = "❌ Submission failed. Try again.";
+      console.error(err);
+    });
+});
+
+
