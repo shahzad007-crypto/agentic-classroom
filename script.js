099236c5cc6e02fe8455b761656fce382ef387ba
shahzad007-crypto <shahzad007_english@yahoo.com>
2026-01-26 18:52:37 +0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById("uploadForm").addEventListener("submit", function(e){
+ e.preventDefault();
+ document.getElementById("msg").innerText =
+ "✅ Submission received! (In real system, file + data stored)";
+});
